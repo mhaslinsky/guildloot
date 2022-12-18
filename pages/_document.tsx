735377fd91bc466a953dc422cd9c464ec58be277
mhaslinsky <mhaslinsky@gmail.com>
2022-12-17 23:20:15 -0500

    init commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang='en'>
       <Head />
       <body>
         <Main />
         <NextScript />
+        <ReactQueryDevtools />
       </body>
     </Html>
-  )
+  );
 }
