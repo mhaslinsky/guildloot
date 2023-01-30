5eb605814f5f2f93e110c0f65151db4721b3d0aa
mhaslinsky <mhaslinsky@gmail.com>
2023-01-30 17:01:43 -0500

    updated next auth and fixed bug with getserversession

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,9 +1,10 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import DiscordProvider from "next-auth/providers/discord";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "../../../prisma/client";
 import EmailProvider from "next-auth/providers/email";
+import { type NextAuthOptions } from "next-auth";
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
--- pages/api/guild.ts
+++ pages/api/guild.ts
@@ -1,18 +1,18 @@
-import { prisma } from "../../prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
-import getServerSession from "next-auth/next";
+import { getServerSession } from "next-auth";
 import { authOptions } from "./auth/[...nextauth]";
 
 export default async function guildEndpoint(req: NextApiRequest, res: NextApiResponse) {
   const session = await getServerSession(req, res, authOptions);
-  console.log(session);
-
+  if (!session) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
   if (req.method == "GET") {
     try {
-      const userId = session.user.id;
-      await prisma.user.findUnique({ where: { id: userId } }).then((data) => {
-        res.status(200).json(data);
-      });
+      res.status(200).json({ message: `User ID:` });
+      // await prisma.user.findUnique({ where: { id: userId } }).then((data) => {
+      //   res.status(200).json(data);
+      // });
     } catch (err) {
       console.log(err);
       res.status(500).json({ message: "error reading from DB" });
